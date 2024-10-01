e2dc0f833c427ea1085003879c4ae08ac6e51bea
james9001 <james9001@james9001.net>
2024-10-02 08:19:35 +1000

    Setup script

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+#Installing some basics
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt -y install nano git neofetch htop nvtop pip nfs-common ncdu
+
+# Installing kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+# Installing kvm hypervisor
+sudo apt -y install libvirt-daemon-system bridge-utils qemu-kvm libvirt-daemon
+
+# Installing minikube itself
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+chmod +x minikube
