846b25916fa43d9b5b1bfad60378f4d3c8d96913
James Osgerby <james.osgerby@checkbox.ai>
2024-10-03 13:21:55 +1000

    make script noninteractive

--- minikube-docker-setup.sh
+++ minikube-docker-setup.sh
@@ -18,8 +18,8 @@ sudo apt -y install nano git neofetch htop nvtop pip nfs-common ncdu
 
 #Install docker
 # Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get -y update
+sudo apt-get -y install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -28,8 +28,8 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get -y update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Installing minikube itself
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
